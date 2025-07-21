39ba58f058a82345fd8a3b744dddbba61b7dec19
Miguel Mota <mota.m.miguel@gmail.com>
2025-07-21 23:32:20 +0100

    Add contracts to Mint/Flip Mood nft

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,12 +2,13 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
 
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
 import {ServerConstants} from "../script/ServerConstants.s.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is ServerConstants, Script {
 
@@ -23,5 +24,41 @@ contract MintBasicNft is ServerConstants, Script {
         address mostRecentlyDeployedBasicNft = DevOpsTools
             .get_most_recent_deployment("BasicNft", block.chainid);
         mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+}
+
+contract MintMoodNft is Script {
+
+    function mintNftOnContract(address moodNftAddress) public {
+        MoodNft moodNft = MoodNft(moodNftAddress);
+
+        vm.startBroadcast();
+        moodNft.mintNft();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+}
+
+contract FlipMoodNft is Script {
+
+    uint256 tokenId = vm.envOr("TOKEN_ID", uint256(0));
+
+    function flipNftOnContract(address moodNftAddress) public {
+        MoodNft moodNft = MoodNft(moodNftAddress);
+
+        vm.startBroadcast();
+        moodNft.flipMood(tokenId);
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        flipNftOnContract(mostRecentlyDeployedBasicNft);
     }
 }
