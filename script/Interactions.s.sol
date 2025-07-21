3850cbf424bf9b897d33a5025bff3a38c126f194
Miguel Mota <mota.m.miguel@gmail.com>
2025-07-21 23:01:33 +0100

    Store PUG_URI in separate constants contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,11 +6,10 @@ import {Script} from "forge-std/Script.sol";
 
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
+import {ServerConstants} from "../script/ServerConstants.s.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
-contract MintNft is Script {
-    string public constant PUG_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+contract MintBasicNft is ServerConstants, Script {
 
     function mintNftOnContract(address basicNftAddress) public {
         BasicNft basicNft = BasicNft(basicNftAddress);
---
+++ script/ServerConstants.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+abstract contract ServerConstants {
+    string public constant PUG_URI = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+}
