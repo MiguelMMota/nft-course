2fced875c37468dc2d0c1d283ac36dff5047cbbb
Miguel Mota <mota.m.miguel@gmail.com>
2025-07-21 18:33:00 +0100

    Add script to deploy BasicNft

---
+++ script/DeployBasicNft.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract DeployBasicNft is Script {
+    function createBasicNft() public returns (BasicNft) {
+        vm.startBroadcast();
+        BasicNft basicNft = new BasicNft();
+        vm.stopBroadcast();
+
+        return basicNft;
+    }
+
+    function run() public returns (BasicNft) {
+        return createBasicNft();
+    }
+}
